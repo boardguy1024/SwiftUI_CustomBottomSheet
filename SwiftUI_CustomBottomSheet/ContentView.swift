d4637bf2c192ab1fde0de473daf24a9ed46ca531
boardguy1024 <boardguy1024@gmail.com>
2021-06-26 16:40:50 +0900

    Initial Commit

---
+++ SwiftUI_CustomBottomSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CustomBottomSheet
+//
+//  Created by park kyung seok on 2021/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CustomBottomSheet/SwiftUI_CustomBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomBottomSheetApp.swift
+//  SwiftUI_CustomBottomSheet
+//
+//  Created by park kyung seok on 2021/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
